dae856588fefb7a5487afb49af00578e537b6d9d
razoterhovhannisyan <119690862+razoterhovhannisyan@users.noreply.github.com>
2023-12-29 16:26:25 +0400

    Update urls.py

--- Users/urls.py
+++ Users/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('register/', views.UserRegistrationView.as_view(), name='user-registration'),
     path('login/', views.UserLoginView.as_view(), name='user-login'),
-    # path('google-token/', views.GoogleTokenCode.as_view(), name='google-token'),
-    # path('facebook-token/', views.FacebookTokenCode.as_view(), name='facebook-token'),
-
 ]
